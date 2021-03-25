ca8c4c89e295348f7465a178d29f8838fe23f7e1
Joey Meisner <emptygrocerybag@hotmail.com>
2021-03-25 11:05:54 -0400

    clean up

--- Crypt.java
+++ Crypt.java
@@ -1,27 +1,11 @@
 //Util
 import java.util.Scanner;
 import java.util.Arrays;
-import java.util.Base64;
-
-//I/O
-import java.io.InputStream;
-import java.io.BufferedInputStream;
-import java.io.OutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
-//Maps
-import java.util.LinkedHashMap;
 
 //Security
 import java.security.Key;
 import java.security.PrivateKey;
-import java.security.KeyFactory;
-import java.security.Security;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 import java.security.spec.KeySpec;
 import java.security.InvalidKeyException;
 import java.security.spec.AlgorithmParameterSpec;
@@ -39,39 +23,9 @@ import javax.crypto.spec.IvParameterSpec;
 
 public class Crypt
 {
-	public static void main(String[] args)
-	{
-		System.out.println("\nEnter password");
-		char[] pw = System.console().readPassword();
-
-		// random salt and IV
-		SecureRandom rand = new SecureRandom();
-
-		byte[] initVector = new byte[16];
-		rand.nextBytes(initVector);
-
-		byte[] seed = rand.generateSeed(16);
-		rand.nextBytes(seed);
-
-		AlgorithmParameterSpec ivspec = new IvParameterSpec(initVector);
-
-		try{
-			//(byte[] password, byte[] salt, int iterationCount, int keyLength)
-			KeySpec pwkeyspec = new PBEKeySpec(pw, seed, 128 * 8, 128);
-			SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBEWithHmacSHA256AndAES_128");
-			SecretKey key = keyFactory.generateSecret(pwkeyspec);
-
-			AlgorithmParameterSpec pwparams = new PBEParameterSpec(seed, 128 * 8, ivspec);
-
-			String message = "Super secret message :D";
-			byte[] encryptedMessage = encryptStringAES(message, key, pwparams, ivspec);
-			String decryptedMessage = decryptStringAES(encryptedMessage);
-		} catch(Exception e) { e.printStackTrace(); }
-	}
-
 	/**
-	 * encryptStringAES - Encrypts a string using AES and a password. After the 
-	 * string is encrypted, the initialization vector and salt is appended.
+	 * encryptStringAES - Encrypts a byte array using AES and a password. After
+	 * the string is encrypted, the initialization vector and salt is appended.
 	 *
 	 * @param inputString		the string to be encrypted
 	 * @param key						AES key
@@ -81,48 +35,53 @@ public class Crypt
 	 * @return 							encrypted bytes
 	 * **/
 	public static byte[] encryptStringAES(
-		String inputString,
+		byte[] inputString,
 		Key key, 
 		AlgorithmParameterSpec pwparams,
 		AlgorithmParameterSpec ivspec) throws Exception
 	{
-		byte[] string = Base64.getEncoder().encode(toByteArray(inputString));
-		byte[] header = appendByteArray(((PBEParameterSpec)pwparams).getSalt(), ((IvParameterSpec)ivspec).getIV());
-
-		// this is the actual encryption process
 		Cipher encrypt = Cipher.getInstance("PBEWithHmacSHA256AndAES_128");
 		encrypt.init(Cipher.ENCRYPT_MODE, key, pwparams);
 
-		byte[] encryptedString = encrypt.doFinal(string);
+		byte[] encryptedString = encrypt.doFinal(inputString);
 
-		return appendByteArray(header, encryptedString); 
+		byte[] header = appendByteArray(
+				((PBEParameterSpec)pwparams).getSalt(), 
+				((IvParameterSpec)ivspec).getIV()
+				);
+		return appendByteArray(header, encryptedString);
 	}
 
-	public static String decryptStringAES(byte[] inputString, Key key, PBEParameterSpec pwparams, IvParameterSpec ivspec) throws Exception
+	/**
+	 * decryptStringAES - Decrypts a byte array. The first 32 bytes of the array
+	 * must be the IV and salt arrays that were used to encrypt the array.
+	 *
+	 * @param inputString		byte array to be decrypted
+	 * 
+	 * @return 							decrypted bytes
+	 * **/
+	public static byte[] decryptStringAES(byte[] inputString) throws Exception
 	{
-		/*// get salt and IV from first 32 bits of array
-		byte[] initVector = new byte[16];
-		for(int i = 0; i < 16; i++)
-			initVector[i] = inputString[i];
-
+		// get salt and IV from first 32 bytes of array
 		byte[] salt = new byte[16];
 		for(int i = 0; i < 16; i++)
-			salt[i] = inputString[i + 16];
+			salt[i] = inputString[i];
 
-		// truncate salt and IV to prepare string for decryption
+		byte[] initVector = new byte[16];
+		for(int i = 0; i < 16; i++)
+			initVector[i] = inputString[i + 16];
+		
+		// remove IV and salt from string
 		byte[] truncated = new byte[inputString.length - 32];
 		for(int i = 32; i < inputString.length; i++)
 			truncated[i - 32] = inputString[i];
-
-		// encode as Base64 or else the cipher will bark at us...
-		byte[] b64String = Base64.getEncoder().encode(truncated);
-		*/
 
 		// @TODO HACK HACK HACK
 		Cipher decrypt = null;
 
 		try{
-			// @TODO don't hardcode password...
+			// @TODO MAKE KEY
+			/*
 			KeySpec pwkeyspec = new PBEKeySpec("156ab2gdb".toCharArray(), salt, 128 * 8, 128);
 
 			AlgorithmParameterSpec ivspec = new IvParameterSpec(initVector);
@@ -136,8 +95,9 @@ public class Crypt
 			decrypt.init(Cipher.DECRYPT_MODE, key, pwparams);
 		} catch(Exception e) { e.printStackTrace(); }		
 
-		return (decrypt.doFinal(b64String)).toString();
+		return decrypt.doFinal(truncated);
 	}
+	*/
 
 	public static void printByteArray(byte[] a)
 	{
